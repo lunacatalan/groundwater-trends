88f26f95a46bcf60d5b09f2421f8318b2799bff5
lunacatalan <lunahcatalan@gmail.com>
2024-04-09 23:06:46 -0700

    updated rolling_lm function so that it is calculatig 5 year slope

--- funs/rolling_lm.R
+++ funs/rolling_lm.R
@@ -11,25 +11,34 @@
 
 rolling_lm <- function(data, window_size) {
   
-  results <- data %>%
-    mutate(rolling_index = row_number()) %>%
-    group_by(rolling_index) 
+  years <- c(data$year)
   
   coef_df <- data.frame()
   
-  for(start_year in seq_along(results$year)) {
+  for(start_year in seq_along(years)) {
     
-    start_date <- results$year[start_year]
+    start_date <- years[start_year]
+    
     end_year <- start_date + window_size
-    window_data <- subset %>%
-      filter(year >= start_year & year <= end_year)
-    lm_results <- lm(DepthToWater_m ~ year, data = window_data)
+    
+    window_data <- results %>%
+      filter(year >= start_date & year <= end_year)
+    
+    # Check if there are any NA values in DepthToWater_m
+    if (any(is.na(window_data$DepthToWater_m))) {
+      next
+    }
+    
+    lm_results <- lm(DepthToWater_m ~ year, 
+                     data = window_data)
+    
     slope <- summary(lm_results)$coefficients[2] # get the slope
+    
     df <- data.frame(start_date = start_date, 
-                     end_date = end_year, slope) 
+                     end_date = end_year, 
+                     slope = slope) 
     
     coef_df <- rbind(coef_df, df)
-    
   }
   
   return(coef_df)
